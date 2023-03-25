697a8bc64007e93bfc6501beea14a17d2b9a62da
Lupu Daniel <dlupu666@gmail.com>
2023-03-25 12:08:06 +0200

    Task_4

---
+++ Task_4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program {
+    static void Main() {
+        Console.Write("Введите первое число: ");
+        int num1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите второе число: ");
+        int num2 = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите третье число: ");
+        int num3 = int.Parse(Console.ReadLine());
+
+        int max = num1;
+        if (num2 > max) {
+            max = num2;
+        }
+        if (num3 > max) {
+            max = num3;
+        }
+
+        Console.WriteLine($"Максимальное число: {max}");
+    }
+}
+
