2a313ea6b18e6800f35d8505ce260b44531a06b3
Lupu Daniel <dlupu666@gmail.com>
2023-03-25 12:09:51 +0200

    Task_6

---
+++ Task_6/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program {
+    static void Main() {
+        Console.Write("Введите число: ");
+        int num = int.Parse(Console.ReadLine());
+
+        if (num % 2 == 0) {
+            Console.WriteLine("Число четное");
+        }
+        else {
+            Console.WriteLine("Число нечетное");
+        }
+    }
+}
