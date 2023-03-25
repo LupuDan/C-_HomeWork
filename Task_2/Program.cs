fc3b22b22b69bc69ab769665ffba8eaef7fa5f78
Lupu Daniel <dlupu666@gmail.com>
2023-03-25 12:06:12 +0200

    Task_2

---
+++ Task_2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program {
+    static void Main() {
+        Console.Write("Введите первое число: ");
+        int num1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите второе число: ");
+        int num2 = int.Parse(Console.ReadLine());
+
+        if (num1 > num2) {
+            Console.WriteLine($"{num1} больше, чем {num2}");
+        }
+        else if (num2 > num1) {
+            Console.WriteLine($"{num2} больше, чем {num1}");
+        }
+        else {
+            Console.WriteLine($"{num1} и {num2} равны");
+        }
+    }
+}
+
